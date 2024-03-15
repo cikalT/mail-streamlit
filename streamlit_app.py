e60536847a11faa072b1df8ebe084617a12b1238
Cikal T <cikaltaruna@gmail.com>
2024-03-15 12:52:11 +0800

    add config

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,25 @@ import streamlit as st
 import requests
 from datetime import datetime
 
+tokens =  st.secrets["TOKENS"]
+namespaces = st.secrets["NAMESPACES"]
 
-# Make a request to fetch the JSON data
-response = requests.get("https://api.testmail.app/api/json?apikey=bfe23c1a-a2b4-49a6-95e5-4b39e27de424&namespace=84z4f&pretty=true")
+response = requests.get(f"https://api.testmail.app/api/json?apikey={tokens}&namespace={namespaces}&pretty=true")
 data = response.json()
+
+st.set_page_config(layout="wide")
+
+st.header('EMAIL TESTING', divider='rainbow')
+st.subheader('How to use:')
+st.caption(f'{namespaces}.{{anything}}@inbox.testmail.app')
 
 for email in data["emails"]:
     st.write(f"From: {email['from']}")
     st.write(f"To: {email['to']}")
     st.write(f"Subject: {email['subject']}")
     
-    # Convert timestamp to Day/Month/Year format
     date_str = datetime.utcfromtimestamp(email['date'] / 1000).strftime('%d/%m/%Y')
     st.write(f"Date: {date_str}")
+    st.components.v1.html(email['html'], height=1000, scrolling=True)
     
-    st.components.v1.html(email['html'], height=500, scrolling=True)  # Render HTML content
-    
-    # Add a separator between emails
     st.write('---')
