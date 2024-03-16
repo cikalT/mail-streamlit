e292cad16435ade0d5a7d538725885155447760c
Cikal T <cikaltaruna@gmail.com>
2024-03-16 17:27:08 +0800

    offset

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,12 @@ import streamlit as st
 import requests
 from datetime import datetime
 
-def get_email_data(tokens, namespaces):
-    response = requests.get(f"https://api.testmail.app/api/json?apikey={tokens}&namespace={namespaces}&pretty=true&limit=100")
+def get_email_data(tokens, namespaces, offset=0):
+    response = requests.get(f"https://api.testmail.app/api/json?apikey={tokens}&namespace={namespaces}&pretty=true&limit=100&offset={offset}")
     return response.json()
 
 tokens = st.secrets["TOKENS"]
 namespaces = st.secrets["NAMESPACES"]
-
-data = get_email_data(tokens, namespaces)
 
 st.set_page_config(layout="wide")
 
@@ -18,11 +16,20 @@ st.subheader('How to use:')
 st.caption(f'{namespaces}.{{use anything in this section}}@inbox.testmail.app')
 
 emails_by_recipient = {}
-for email in data["emails"]:
-    recipient = email['to']
-    if recipient not in emails_by_recipient:
-        emails_by_recipient[recipient] = []
-    emails_by_recipient[recipient].append(email)
+
+offset = 0
+while True:
+    data = get_email_data(tokens, namespaces, offset)
+    for email in data["emails"]:
+        recipient = email['to']
+        if recipient not in emails_by_recipient:
+            emails_by_recipient[recipient] = []
+        emails_by_recipient[recipient].append(email)
+
+    if len(data["emails"]) < 100:
+        break
+    else:
+        offset += 100
 
 left_column, right_column = st.columns([1, 3])
 
@@ -32,7 +39,6 @@ with left_column:
         recipient_button_label = f"To: {recipient} ({len(emails)} emails)"
         if st.button(recipient_button_label):
             selected_recipient_emails = emails
-            data = get_email_data(tokens, namespaces)
 
 with right_column:
     if selected_recipient_emails is not None:
@@ -44,4 +50,3 @@ with right_column:
             date_str = datetime.utcfromtimestamp(email['date'] / 1000).strftime('%d/%m/%Y')
             st.write(f"Date: {date_str}")
             st.components.v1.html(email['html'], height=1000, scrolling=True)
-
